721ffb9b081f2a5bbc5b995f715f90666aed5463
NDaly94 <NDaly94@gmail.com>
2023-06-06 16:33:14 -0400

    sever updated, schema added

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
-const express = require('express');
+const express = require('express')
+const mongoose = require('mongoose')
+const fs = require('fs')
 
-const app = express();
+const app = express()
+const Item = require('./models/item')
+
+
 const PORT = 3000;
 
 //GET 
 app.get('/items', async (req, res) => {
     try{
-
+        const foundItems = await Item.find({})
+        res.render('items/index', {
+            items: foundItems
+        })
     } catch (error) {
+        res.status(400).send({ message: error.message})
 
 })
 //POST
@@ -15,29 +24,40 @@ app.post('/items', async (req, res) => {
     try {
 
     } catch (error) {
+        res.status(400).send({ message: error.message})
 
 })
 
-//GET by ID
+//NEW
 app.get('/items/:id', async (req, res) => {
 try {
-
+    res.render('items/:id')
 } catch (error) {
+    res.status(400).send({ message: error.message})
 
 })
 
 //PUT by ID
-app.put('/items/:id', asnyc (req, res) => {
+app.put('/items/:id', async (req, res) => {
     try {
+        await Item.findOneAndUpdate({'_id': req.params.id},
+        req.body, { new: true })
+        .the(() => {
+            res.redirect(`/items/${req.params.id}`)
+        })
 }catch(error){
-
+    res.status(400).send({ message: error.message})
 })
 
 //DELETE by ID
 app.delete('/items/:id', async (req, res) => {
     try {
+        await Item.findOneAndDelete({'_id': req.params.id})
+        .then(() => {
+            res.redirect('/items')
+        })
 }catch(error){
-
+    res.status(400).send({ message: error.message})
 })
 
 app.listen(PORT, () => {
